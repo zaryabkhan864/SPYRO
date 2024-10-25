8caf925439331273c70c54ed43834256c66c3c15
Muhammad Zaryab Khan <zaryabkhan864@gmail.com>
2024-10-25 23:55:16 +0300

    FAQs bugs fixed...

--- client/src/components/FAQS/FAQS.jsx
+++ client/src/components/FAQS/FAQS.jsx
@@ -50,72 +50,73 @@ const FAQS = () => {
     return (
         <div className="relative font-inter antialiased bg-[#1E1E1E] text-white">
             <main className="relative min-h-screen flex flex-col justify-center overflow-hidden">
-                <div className="w-full px-48 mx-auto md:py-24">
+                <div className="w-full px-44 mx-auto md:py-24">
                     <h1 className="font-Outfit font-semibold text-40px mb-4 text-center">Frequently Asked Questions</h1>
 
                     <div className="divide-y divide-slate-200">
                         {faqsData.map((faq) => (
-                            <div>
-                                <div key={faq.id} className="py-2 relative">
-                                    <h2>
-                                        <button
-                                            id={`faqs-title-${faq.id}`}
-                                            type="button"
-                                            className="flex items-center justify-between w-full text-left font-semibold py-2"
-                                            onClick={() => toggleExpanded(faq.id)}
-                                            aria-expanded={expanded === faq.id}
-                                            aria-controls={`faqs-text-${faq.id}`}
+                            <div key={faq.id} className="py-4">
+                                <h2>
+                                    <button
+                                        id={`faqs-title-${faq.id}`}
+                                        type="button"
+                                        className="flex items-center justify-between w-full text-left font-semibold py-2"
+                                        onClick={() => toggleExpanded(faq.id)}
+                                        aria-expanded={expanded === faq.id}
+                                        aria-controls={`faqs-text-${faq.id}`}
+                                    >
+                                        <span className="font-Outfit font-semibold text-24px">{faq.question}</span>
+                                        <svg
+                                            className="fill-indigo-500 shrink-0 ml-8"
+                                            width="16"
+                                            height="16"
+                                            xmlns="http://www.w3.org/2000/svg"
                                         >
-                                            <span className="font-Outfit font-semibold text-24px">{faq.question}</span>
-                                            <svg
-                                                className="fill-indigo-500 shrink-0 ml-8"
+                                            <rect
+                                                y="7"
                                                 width="16"
-                                                height="16"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                            >
-                                                <rect
-                                                    y="7"
-                                                    width="16"
-                                                    height="2"
-                                                    rx="1"
-                                                    className={`transform origin-center transition duration-200 ease-out ${expanded === faq.id ? '!rotate-180' : ''}`}
-                                                />
-                                                <rect
-                                                    y="7"
-                                                    width="16"
-                                                    height="2"
-                                                    rx="1"
-                                                    className={`transform origin-center rotate-90 transition duration-200 ease-out ${expanded === faq.id ? '!rotate-180' : ''}`}
-                                                />
-                                            </svg>
-                                        </button>
-                                    </h2>
-
-                                </div>
+                                                height="2"
+                                                rx="1"
+                                                className={`transform origin-center transition duration-200 ease-out ${expanded === faq.id ? '!rotate-180' : ''}`}
+                                            />
+                                            <rect
+                                                y="7"
+                                                width="16"
+                                                height="2"
+                                                rx="1"
+                                                className={`transform origin-center rotate-90 transition duration-200 ease-out ${expanded === faq.id ? '!rotate-180' : ''}`}
+                                            />
+                                        </svg>
+                                    </button>
+                                </h2>
                                 <div
                                     id={`faqs-text-${faq.id}`}
                                     role="region"
                                     aria-labelledby={`faqs-title-${faq.id}`}
-                                    className={`relative grid text-sm text-slate-600 overflow-hidden transition-all duration-300 ease-in-out ${expanded === faq.id ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}
+                                    className={`relative transition-all duration-300 ease-in-out overflow-hidden ${expanded === faq.id ? 'max-h-[200px] border-t border-slate-200 mt-2' : 'max-h-0'
+                                        }`}
                                 >
-                                    <div className="overflow-hidden text-white p-3 bg-orangeL relative transform -skew-x-12 ">
-                                        <p >{faq.answer}</p>
+                                    <div className="p-4 relative transform -skew-x-12">
+                                        <div
+                                            className="absolute inset-2 bg-gradient-to-t from-orangeL to-orangeR opacity-30"
+                                            style={{ zIndex: -1 }}
+                                        ></div>
+                                        <p className="text-white relative">{faq.answer}</p>
                                     </div>
                                 </div>
                             </div>
-
                         ))}
                     </div>
+
                     <div className="flex flex-row bg-orangeL transform -skew-x-12 py-3 px-9 align-middle">
                         <div className="flex-3">
-                            <h1 className='font-Outfit font-semibold text-40px'>Are you ready to change?</h1>
-                            <p className='font-Outfit font-regular text-16px'>Challenge your strength. Stretch your body. Breathe easy. Our team of world-class instructors will empower</p>
+                            <h1 className="font-Outfit font-semibold text-40px">Are you ready to change?</h1>
+                            <p className="font-Outfit font-regular text-16px">Challenge your strength. Stretch your body. Breathe easy. Our team of world-class instructors will empower</p>
                         </div>
                         <div className="flex-2 flex justify-end items-center">
                             <OneSideCurveButtonWithOutline className="w-3/4 lg:w-1/2" />
                         </div>
                     </div>
-
                 </div>
             </main>
         </div>
