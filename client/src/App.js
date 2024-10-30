f4a435403b997f415e5f9ed48e363a3e6ec73670
Muhammad Zaryab Khan <zaryabkhan864@gmail.com>
2024-10-30 23:40:26 +0300

    add new component , classes Details, fixed button font style ..

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Banner from './components/Banner/Banner';
 import Classess from './components/Classess/Classess';
-import DownloadPart from './components/DownloadPart/DownloadPart';
-import Feedback from './components/FeedBack/Feedback';
+
 import Instructors from './components/Instructors/Instructors';
-import LifeStyle from './components/LifeStyle/LifeStyle';
-import Moves from './components/Moves/Moves';
+
 import SNavbar from './components/Navbar/SNavbar';
 import VideoPart from './components/Videopart/VideoPart';
-import FAQS from './components/FAQS/FAQS';
+
 import Footer from './components/Footer/Footer';
 import HomeComponent from './components/FAQS/HomeComponent';
 import Strength from './components/ClassesDetails/Strength';
